7e75ae4aa66707d885212f37c4e5eb9dc880e064
Juliano <juliano.lopes@mackenzista.com.br>
2018-03-28 15:47:28 -0300

    Autenticação ok, prox email 2

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -122,7 +122,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-
-
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
