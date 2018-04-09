da0e74a2a020c56b6f120d6a3d049e7ee3294ff7
Juliano Moraes <troublie@gmail.com>
2018-04-09 20:17:20 -0300

    testes refatorando

--- myproject/fuji/tests/test_views.py
+++ myproject/fuji/tests/test_views.py
@@ -137,26 +137,27 @@ class CadatroPedidoTest(TestCase):
         url = reverse_lazy('home')
         self.assertContains(self.response, 'href="{0}"'.format(url))
 
+
 class CadastroPedidoSucesso(TestCase):
     def setUp(self):
         self.moeda = Moeda.objects.create(simbolo="USD", descricao="DOLAR")
         self.prioridade = Prioridade.objects.create(tipo="Alta")
         self.tipo_emb = Tipo_emb.objects.create(descricao="Spare parts")
         self.fornecedor = Fornecedor.objects.create(nome="Jabil", endreco="Teste", email_contato="teste@gmail.com",
-                                               nome_contato="Joao")
+                                                    nome_contato="Joao")
         self.termo = Termo.objects.create(dias="30")
         self.customer = Customer.objects.create(nome="Sojitz", endreco="Teste", email_contato="teste2@gmail.com",
-                                               nome_contato="Joao")
+                                                nome_contato="Joao")
         self.user = User.objects.create_user(username='john', email='john@doe.com', password='old_password')
 
     def test_cadastro_sucesso(self):
         data = {
-            'customer':"1", 'received_date':"27/01/2018", 'fornecedor':"1",
-            'date_sent_vendor':"27/01/2018", 'number':"12345", 'proforma':"12345",
-            'invoice':"12345", 'instrucoes':"DHL", 'embarcado_finalizado_em':"27/02/2018",
-            'awb':"12345", 'tracking':"123456", 'moeda':"1", 'amount_total':"1200",
-            'responsavel_fdb':"1", 'tipo_embarque':"1",
-            'termo_pagto':"1", 'vencimento':"27/01/2019", 'pago':"1", 'prioridade':"1"
+            'customer': "1", 'received_date': "27/01/2018", 'fornecedor': "1",
+            'date_sent_vendor': "27/01/2018", 'number': "12345", 'proforma': "12345",
+            'invoice': "12345", 'instrucoes': "DHL", 'embarcado_finalizado_em': "27/02/2018",
+            'awb': "12345", 'tracking': "123456", 'moeda': "1", 'amount_total': "1200",
+            'responsavel_fdb': "1", 'tipo_embarque': "1",
+            'termo_pagto': "1", 'vencimento': "27/01/2019", 'pago': "1", 'prioridade': "1"
         }
 
         url = reverse_lazy('cadastro_pedido')
@@ -166,15 +167,32 @@ class CadastroPedidoSucesso(TestCase):
 
     def test_cadastro_insucesso(self):
         data = {
-            'customer':"5", 'received_date':"27/01/2018", 'fornecedor':"1",
-            'date_sent_vendor':"27/01/2018", 'number':"12345", 'proforma':"12345",
-            'invoice':"12345", 'instrucoes':"DHL", 'embarcado_finalizado_em':"27/02/2018",
-            'awb':"12345", 'tracking':"123456", 'moeda':"1", 'amount_total':"1200",
-            'responsavel_fdb':"5", 'tipo_embarque':"1",
-            'termo_pagto':"1", 'vencimento':"27/01/2019", 'pago':"1", 'prioridade':"1"
+            'customer': "5", 'received_date': "27/01/2018", 'fornecedor': "1",
+            'date_sent_vendor': "27/01/2018", 'number': "12345", 'proforma': "12345",
+            'invoice': "12345", 'instrucoes': "DHL", 'embarcado_finalizado_em': "27/02/2018",
+            'awb': "12345", 'tracking': "123456", 'moeda': "1", 'amount_total': "1200",
+            'responsavel_fdb': "5", 'tipo_embarque': "1",
+            'termo_pagto': "1", 'vencimento': "27/01/2019", 'pago': "1", 'prioridade': "1"
         }
 
         url = reverse_lazy('cadastro_pedido')
         response = self.client.post(url, data)
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'form-group', 19)
+
+
+class PedidoListViewTest(TestCase):
+    def setUp(self):
+        url = reverse_lazy('lista_pedido')
+        self.response = self.client.get(url)
+
+    def test_pedido_list_success_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_pedido_list_view_has_link_to_homepage(self):
+        url_homepage = reverse_lazy('home')
+        self.assertContains(self.response, url_homepage)
+
+    def test_lista_pedido_url_resolve_lista_pedido_view(self):
+        view = resolve('/lista_pedido/')
+        self.assertEquals(view.func, lista_pedido)
