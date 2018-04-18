9ab2e0740f717f7776365cce40f24c777c0107cf
Juliano <juliano.lopes@mackenzista.com.br>
2018-04-18 16:16:41 -0300

    Step 1

--- myproject/fuji/forms.py
+++ myproject/fuji/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.forms import DateInput, DateField
-from django.utils import translation
+from django.forms import DateInput
+
 
 from .models import Order
 
@@ -21,6 +21,12 @@ class NewOrderForm(forms.ModelForm):
             'date_sent_vendor': DateInput(),
             'embarcado_finalizado_em': DateInput()
         }
+
+class SearchForm(forms.Form):
+    q = forms.CharField(max_length=100)
+
+    class Meta:
+        fields = ['q']
 
 
 
